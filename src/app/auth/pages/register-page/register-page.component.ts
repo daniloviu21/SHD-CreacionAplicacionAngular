091ced2ab4cf2cfb4d9534fc985f2b48f84088be
daniloviu21 <daniel.darkwatch@gmail.com>
2025-02-12 00:27:26 -0600

    feat: agregar registro

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
@@ -8,4 +9,35 @@ import { Component } from '@angular/core';
 })
 export class RegisterPageComponent {
 
+  registerForm: FormGroup;
+  passwordMismatch: boolean = false;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    });
+
+    this.registerForm.valueChanges.subscribe(() => {
+      this.checkPasswords();
+    });
+  }
+
+  checkPasswords() {
+    const password = this.registerForm.get('password')?.value;
+    const confirmPassword = this.registerForm.get('confirmPassword')?.value;
+    this.passwordMismatch = password !== confirmPassword;
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid && !this.passwordMismatch) {
+      console.log('Formulario enviado:', this.registerForm.value);
+      alert('Registro exitoso 🎉');
+    } else {
+      console.log('Formulario inválido');
+    }
+  }
 }
